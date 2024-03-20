87ec66d83f6bf5de9dce6a6b767a831121686f40
AllanDickerman <dickerman@virginia.edu>
2024-03-20 15:46:21 -0500

    Made splitFiles an optional parameter to download_fastq_files.

--- lib/sra_tools.py
+++ lib/sra_tools.py
@@ -262,7 +262,7 @@ def get_run_ids(run_meta):
         run_ids.append(run['run_id'])
     return run_ids
 
-def download_fastq_files(fasterq_dump_loc, output_dir, metadata, gzip_output=True):
+def download_fastq_files(fasterq_dump_loc, output_dir, metadata, gzip_output=True, splitFiles=False):
 
     #
     # determine the list of ids to download
@@ -296,7 +296,9 @@ def download_fastq_files(fasterq_dump_loc, output_dir, metadata, gzip_output=Tru
             tmpdir = os.getenv("TMPDIR")
             if tmpdir is None:
                 tmpdir = "/tmp"
-            fasterq_dump_cmd = [fasterq_dump_loc, '-t', tmpdir, '--outdir', output_dir,  '--split-files', '-f', run_id]
+            fasterq_dump_cmd = [fasterq_dump_loc, '-t', tmpdir, '--outdir', output_dir, '-f', run_id]
+            if splitFiles: #optional, not needed, --split-3 is the default (see https://github.com/ncbi/sra-tools/wiki/HowTo:-fasterq-dump)
+                fasterq_dump_cmd.append('--split-files')
 
         try:
             # print >> sys.stderr, 'executing \'' + str(fasterq_dump_cmd) + '\''
